491a0371f075896e26c5007da82ecc5ea7128946
DiegoLorBal <dielorbal@gmail.com>
2023-06-27 14:50:54 +0200

    segunda version

--- src/app/karma-jasmine.service.spec.ts
+++ src/app/karma-jasmine.service.spec.ts
@@ -28,4 +28,46 @@ describe('KarmaJasmineService', () => {
     expect(resultado).toBe(5);
   });
 
+  it('debería restar dos números correctamente', () => {
+    const resultado = service.restar(2, 3);
+    expect(resultado).toBe(-1);
+  });
+
+  it('debería restar números negativos correctamente', () => {
+    const resultado = service.restar(-5, -3);
+    expect(resultado).toBe(-2);
+  });
+
+  it('debería restar un número positivo y un número negativo correctamente', () => {
+    const resultado = service.restar(10, -5);
+    expect(resultado).toBe(15);
+  });
+  it('debería multiplicar dos números correctamente', () => {
+    const resultado = service.multiplicar(2, 3);
+    expect(resultado).toBe(6);
+  });
+
+  it('debería multiplicar números negativos correctamente', () => {
+    const resultado = service.multiplicar(-5, -3);
+    expect(resultado).toBe(15);
+  });
+
+  it('debería multiplicar un número positivo y un número negativo correctamente', () => {
+    const resultado = service.multiplicar(10, -5);
+    expect(resultado).toBe(-50);
+  });
+  it('debería dividir dos números correctamente', () => {
+    const resultado = service.dividir(10, 2);
+    expect(resultado).toBe(5);
+  });
+
+  it('debería dividir números negativos correctamente', () => {
+    const resultado = service.dividir(-10, -2);
+    expect(resultado).toBe(5);
+  });
+
+  it('debería dividir un número positivo y un número negativo correctamente', () => {
+    const resultado = service.dividir(10, -5);
+    expect(resultado).toBe(-2);
+  });
 });
--- src/app/karma-jasmine.service.ts
+++ src/app/karma-jasmine.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class KarmaJasmineService {
-
-  constructor() { }
+  constructor() {}
 
   sumar(a: number, b: number): number {
     return a + b;
   }
-
-
+  restar(a: number, b: number): number {
+    return a - b;
+  }
+  multiplicar(a: number, b: number): number {
+    return a * b;
+  }
+  dividir(a: number, b: number): number {
+    return a / b;
+  }
 }
